89000668b92a8eafa4e5c1a422c339ba34d60653
Jacob Campbell <jscampbell21@outlook.com>
2020-01-12 15:18:42 -0500

    My first upload

---
+++ Campbell 01/src/Campbell_01/Example01.java
@@ -0,0 +1,18 @@
+package Campbell_01;
+
+import java.util.Scanner;
+
+public class Example01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		
+		System.out.println("Please enter your name: ");
+		String name = in.next();
+		
+		System.out.println("Hello, " + name);
+	}
+
+}
